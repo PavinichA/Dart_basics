c3320bafc014cb7dd26ee81aa7e937a1c50ea7f5
PavinichA <111359999+PavinichA@users.noreply.github.com>
2022-09-09 18:30:28 +0300

    Add files via upload
    Правки

--- flutter_application_2_test.dart
+++ flutter_application_2_test.dart
@@ -4,16 +4,48 @@ import 'package:flutter_application_2/flutter_application_2.dart';
 
 void main() {
 // task 1
-int gcd(int a, int b);
-int lcm(int a, int b);
-List<int> factors(int number);
+int gcd(int a, int b) {
+  if (a == b)
+  return a;
+  if (a > b)
+  return gcd(a-b, b);
+  return gcd(a, b-a);
+}
+int lcm(int a, int b) {
+  return (a*b) / gcd(a, b);
+}
+List<int> factors(int number) {
+    if (number < 2) return [];
+    int factor = 2;
+    List<int> lstFactors = [];
+    while (number / factor != 1) {
+      if (number % factor == 0) {
+        lstFactors.add(factor);
+        number = number ~/ factor;
+        factor = 2;
+      } else {
+         factor++;
+      }
+    }
+    ;
+    lstFactors.add(factor);
+    return lstFactors;
+  }
+}
 // task 2
-String intToBinary(int number);
-int binaryToInt(String binary);
+//число всегда целое
+static String decimalToBinary(int number) {
+  return number.toRadixString(2);
+}
+//двоичный файл всегда находится в строке
+static int binaryToDecimal(String binary) {
+  return int.parse(binary, radix: 2);
+}
 // task 3
-List<num> findNumsIn(String string);
+List<num> findNumsIn(String string) 
+}
 // task 4
-Map<String, int> findStringsIn(List<String> stringList);
+Map<String, int> findStringsIn(List<String> stringList) 
 // task 5
 Set<int> findNumNamesIn(List<String> stringList);
 // task 6
@@ -22,11 +54,6 @@ distanceTo(Point other) {
   var dx = x - other.x; 
   var dy = y - other.y; 
   var dz = z - other.z;
-  return sqrt(dx * dx + dy * dy + dz * dz); 
-} 
+  return sqrt(dx * dx + dy * dy + dz * dz);
 }
-// task 7
-num.rootPower(x);
-// task 8
-List<String> getEmails();
 }
